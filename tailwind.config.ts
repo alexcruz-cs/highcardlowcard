cff53ae93216841faefa32ef0e22190a146b86ff
Steven Barsam <sbarsam@riotgames.com>
2024-09-30 18:35:56 -0700

    Add some flip animations and make the width good on mobile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,35 +15,60 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
 
-      
-
       keyframes: {
         wiggle: {
-          '0%, 100%': {
-              transform: 'rotate(-1deg)'
+          "0%, 100%": {
+            transform: "rotate(-1deg)",
           },
-          '50%': {
-              transform: 'rotate(1deg)'
+          "50%": {
+            transform: "rotate(1deg)",
           },
-
-          cardflip : {
-            '50%' : 'rotateY(180deg)'
-          },
-
         },
-
+        cardflip: {
+          "0%": {
+            content: "?",
+            transform: "rotateY(-180deg)",
+          },
+          "100%": {
+            transform: "rotateY(0deg)",
+          },
+        },
+        showThenHide: {
+          "0%": {
+            opacity: "1",
+          },
+          "99%": {
+            opacity: "1",
+          },
+          "100%": {
+            opacity: "0",
+            position: "absolute",
+          },
+        },
+        hideThenShow: {
+          "0%": {
+            opacity: "0",
+            position: "absolute",
+          },
+          "99%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+            position: "block",
+          },
+        },
       },
       animation: {
-        wiggle: 'wiggle 1s ease-in-out infinite',
-        cardflip: 'cardflip 1s ease-in-out infinite'
+        wiggle: "wiggle 1s ease-in-out infinite",
+        cardflip: "cardflip .5s ease-in-out",
+        showThenHide: "showThenHide .25s forwards",
+        hideThenShow: "hideThenShow .25s forwards",
       },
-      
     },
   },
 
   plugins: [],
-  
-
 };
 
 export default config;
